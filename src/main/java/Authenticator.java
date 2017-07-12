685edb73703183d2fabfa6c1b99308b19e10a6b0
Jan Černý <j.cerny.zdar@gmail.com>
2017-07-12 13:47:11 +0200

    Bugfixes of login managing

--- src/main/java/Authenticator.java
+++ src/main/java/Authenticator.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -12,15 +9,17 @@ public class Authenticator {
 
     private Pattern login = Pattern.compile("(\\w+):(.+)");
     private Map<String, String> users;
+    private PrintWriter fileWriter;
 
 
     public Authenticator(String file) {
         try {
             BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            fileWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
             users = new HashMap<>();
             while (true) {
                 String line = fileReader.readLine();
-                if (line != null) {
+                if (line != null && !line.equals("")) {
                     Matcher lineMatcher = login.matcher(line);
                     lineMatcher.find();
                     users.put(lineMatcher.group(1), lineMatcher.group(2));
@@ -33,8 +32,19 @@ public class Authenticator {
         }
     }
 
+    public boolean userIsRegistered(String username) {
+        return users.containsKey(username);
+    }
+
     public boolean authenticate(String descryptedUsername, String descryptedPassword) {
         String hashPassword = ShaUtil.hash(descryptedPassword);
         return hashPassword.equals(users.get(descryptedUsername));
+    }
+
+    public void registerUser(String newUsername, String newPassword) {
+        String hashedPassword = ShaUtil.hash(newPassword);
+        fileWriter.println(newUsername + ":" + hashedPassword);
+        fileWriter.flush();
+        users.put(newUsername, hashedPassword);
     }
 }
