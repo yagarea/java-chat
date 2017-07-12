10a94eb1d4cff5f8894f5ed923e29e387ee2845d
Jan Černý <j.cerny.zdar@gmail.com>
2017-07-12 09:04:52 +0200

    add fix nullPointerExeption (chatServer) on Server

--- src/Server.java
+++ src/Server.java
@@ -10,20 +10,27 @@ public class Server {
         RSA encryptor = new RSA();
         final int PORT = 4077;
 
+        ServerSocket chatServer = null;
         try {
-            ServerSocket chatServer = new ServerSocket(PORT);
-            Map<String, ClientConnection> clients = new HashMap<>();
-            Authenticator auth = new Authenticator();
-            while (true) {
+            chatServer = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Map<String, ClientConnection> clients = new HashMap<>();
+        Authenticator auth = new Authenticator();
+
+
+        while (chatServer != null) {
+            try {
                 Socket clientSocket = chatServer.accept();
                 ClientConnection newClient = new ClientConnection(clientSocket, clients, encryptor, auth);
 
                 new Thread(newClient).start();
                 clients.put(newClient.username, newClient);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
 
+    }
 }
