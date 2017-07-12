a7a2f4f3a20e90eb8e715c033a09a0f564515ea8
Jan Černý <j.cerny.zdar@gmail.com>
2017-07-12 15:26:14 +0200

    add consoleUtilyty (now you can read password safely etc.)

--- src/main/java/com/github/yagarea/chat/client/Client.java
+++ src/main/java/com/github/yagarea/chat/client/Client.java
@@ -1,11 +1,10 @@
 package com.github.yagarea.chat.client;
 
+import com.github.yagarea.chat.shared.ConsoleUtility;
 import com.github.yagarea.chat.shared.LoginResponse;
 import com.github.yagarea.chat.shared.security.RSA;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.net.Socket;
@@ -25,14 +24,14 @@ public class Client {
             BigInteger e = new BigInteger(responsePrinterLoop.readLine());
             BigInteger n = new BigInteger(responsePrinterLoop.readLine());
             RSA encryptor = new RSA(e, n);
-            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            ConsoleUtility consoleReader = new ConsoleUtility();
 
             LoginResponse usernameResponse;
             do {
                 System.out.print("Nickname: ");
                 String username = consoleReader.readLine();
                 System.out.print("Password: ");
-                String password = consoleReader.readLine();
+                String password = consoleReader.readPassword();
                 writer.println(encryptor.encryptString(username));
                 writer.println(encryptor.encryptString(password));
                 writer.flush();
